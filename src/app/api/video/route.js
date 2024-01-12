c8cddf79c0c111dc092a0497c4d778f59adc76a8
Akhilesh Chandewar <AKHIL.CHANDEWAR00@GMAIL.COM>
2024-01-13 01:15:56 +0530

    api error solve

--- src/app/api/video/route.js
+++ src/app/api/video/route.js
@@ -1,17 +1,29 @@
-import clientPromise from "../../../lib/db";
+// video/route.js
 
-export default async function handler(req, res) {
-  const client = await clientPromise;
-  const db = client.db("StreamVista");
-  switch (req.method) {
-    case "POST":
-      let bodyObject = JSON.parse(req.body);
-      let response = await db.collection("video").insertOne(bodyObject);
-      res.json(response.ops[0]);
-      break;
-    case "GET":
-      const allPosts = await db.collection("video").find({}).toArray();
-      res.json({ status: 200, data: allPosts });
-      break;
+import clientPromise from "@/lib/db/index";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("StreamVista");
+    const bodyObject = req.body;
+    const response = await db.collection("video").insertOne(bodyObject);
+    return  NextResponse.json(response);
+  } catch (error) {
+    console.error("Error handling POST request:", error);
+    return NextResponse.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export async function GET(req, res, db) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("StreamVista");
+    const allVideo = await db.collection("video").find({}).toArray();
+    return  NextResponse.json({ status: 200, data: allVideo });
+  } catch (error) {
+    console.error("Error handling GET request:", error);
+    return  NextResponse.status(500).json({ error: "Internal Server Error" });
   }
 }
